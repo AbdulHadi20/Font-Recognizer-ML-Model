32f8987bcc01244059ae67d6c1453a013079f1c6
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2025-01-08 11:29:21 +0400

    .

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Machine Learning Module: Assessment 2 (2025) 
+
+AbdulHadi Aamir
+Creative Computing Year 3
+Bath Spa University, RAK
+
+The following application is a Machine Learning Model for recognizing fonts that are displayed to the program. 
+
+FONT RECOGNIZER MODEL
+
+"""
+
+############ START OF THE PROGRAM ############
+
+# importing all of the required modules and libraries
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt 
+import gradio as gr
